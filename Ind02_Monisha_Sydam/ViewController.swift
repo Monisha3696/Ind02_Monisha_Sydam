9304cd81136a2f4efd9db2a15b321ae710f62e4c
monisha <monisha.sydam@okstate.edu>
2023-03-04 15:51:52 -0600

    Initial Commit

---
+++ Ind02_Monisha_Sydam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ind02_Monisha_Sydam
+//
+//  Created by Monisha Sydam on 3/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
